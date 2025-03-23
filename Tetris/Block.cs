19b8d1de6644593537b5d82047820f5ccea955c5
joaoa <joaoa@NOT_DO_ARIZA>
2025-03-23 12:30:46 -0300

    Adicionar arquivos de projeto.

---
+++ Tetris/Block.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+
+namespace Tetris
+{
+    public abstract class Block
+    {
+        protected abstract Position[][] Tiles { get; }
+        protected abstract Position StartOffset { get; }
+        public abstract int Id { get; }
+
+        private int rotateState;
+        private Position offset;
+
+        public Block()
+        {
+            offset = new Position(StartOffset.Row, StartOffset.Column);
+        }
+
+        public IEnumerable<Position> TilePositions()
+        {
+            foreach (Position p in Tiles[rotateState])
+            {
+                yield return new Position(p.Row + offset.Row, p.Column + offset.Column);
+            }
+        }
+
+        public void RotateCW()
+        {
+            rotateState = (rotateState + 1) % Tiles.Length;
+        }
+
+         public void RotateCCW()
+        {
+            if(rotateState == 0)
+            {
+                rotateState = Tiles.Length - 1;
+            }
+            else
+            {
+                rotateState--;
+            }
+        }
+
+        public void Move(int rows, int columns)
+        {
+            offset.Row += rows;
+            offset.Column += columns;
+        }
+
+        public void Reset()
+        {
+            rotateState = 0;
+            offset.Row = StartOffset.Row;
+            offset.Column = StartOffset.Column;
+        }
+
+    }
+}
